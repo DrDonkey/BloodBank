b5f197e36f1e768e571ffb9c7a45517ac1bb407b
Aasutosh Jha <aasutossh@gmail.com>
2019-07-31 12:08:30 +0545

    added new activity, and linked activity with button

--- app/src/main/java/np/com/aasutosh/bloodbank/LoginActivity.java
+++ app/src/main/java/np/com/aasutosh/bloodbank/LoginActivity.java
@@ -3,6 +3,7 @@ package np.com.aasutosh.bloodbank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void checkDetails(View view) {
+//        TODO: first check details, if already stored phoneID then
     }
 }
